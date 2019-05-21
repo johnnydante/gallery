9c62832b56551914bafd4481265054f888503259
DESKTOP-DMJK3RB\Jasiek <dante.dawid@gmail.com>
2019-05-21 23:31:45 +0200

    title fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Gallery</title>
+    <title>Galeria tortów</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
